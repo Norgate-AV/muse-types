0d997208074234a81afecd5bcef2dbecddc3f38d
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-12-27 23:45:29 +0000

    feat: update definitions
    Add SessionService
    Add NetLinxClientService
    
    Add docs for TimelineService

--- globals.d.ts
+++ globals.d.ts
@@ -126,32 +126,45 @@ declare global {
             source: object;
         }
 
+        /**
+         * The *Timeline* service provides a mechanism for triggering events based on a sequence of times. The sequence of times is pass the *start* function as an array of integer values, with each value representing a time period, in milliseconds, that is either an offset the start of the timeline or relative to the previously triggered event.
+         *
+         * Each call into the context's service requests returns a new instance of a timeline.
+         */
         interface TimelineService {
             /**
              * Starts the timeline
-             * @param intervals Array of time intervals in milliseconds
-             * @param relative If there multiple times in the array, it determines whether the timings are treated as relative delays berween triggers, or as an independent list of times that may trigger out of seuence relative to the order in the list.
-             * @param repeat A value of -1 indicates that the timeline should run forever. A timeline started with a value of 0 will run once. The value indicates the number of repetitions.
-             * @returns void
+             *
+             * @param {Array<number>} intervals Array of time intervals in milliseconds
+             * @param {boolean} [relative = false] If there multiple times in the array, it determines whether the timings are treated as relative delays berween triggers, or as an independent list of times that may trigger out of seuence relative to the order in the list.
+             * @param {number} [repeat = 0] A value of -1 indicates that the timeline should run forever. A timeline started with a value of 0 will run once. The value indicates the number of repetitions.
+             *
+             * @returns {void} void
              */
             start(
                 intervals: Array<number>,
-                relative: boolean,
-                repeat: number,
+                relative?: boolean,
+                repeat?: number
             ): void;
 
             /**
              * Stops the timeline
+             *
+             * @returns {void} void
              */
             stop(): void;
 
             /**
              * Pauses the timeline
+             *
+             * @returns {void} void
              */
             pause(): void;
 
             /**
              * Resumes the timeline from a paused state
+             *
+             * @returns {void} void
              */
             restart(): void;
 
@@ -203,34 +216,199 @@ declare global {
             plugins?: Array<string> | undefined;
         };
 
+        /**
+         * The *Platform* service provides a mechanism to read platform parameters such as model and version.
+         *
+         * @example JavaScript
+         * ```javascript
+         * const platform = context.services.get("platform");
+         *
+         * context.log(`Device State: ${platform.devicestate}`);
+         * context.log(`Location: ${platform.location}`);
+         * context.log(`Name: ${platform.name}`);
+         * context.log(`Version: ${platform.version}`);
+         * context.log(`Venue: ${platform.venue}`);
+         * context.log(`Description: ${platform.description}`);
+         * context.log(`Serial Number: ${platform.serialnumber}`);
+         * context.log(`Family: ${platform.family}`);
+         * context.log(`Manufacturer: ${platform.manufacturer}`);
+         * context.log(`Model: ${platform.model}`);
+         * context.log(`Label: ${platform.label}`);
+         * ```
+         * @example TypeScript
+         * ```typescript
+         * const platform = context.services.get<Muse.PlatformService>("platform");
+         *
+         * context.log(`Device State: ${platform.devicestate}`);
+         * context.log(`Location: ${platform.location}`);
+         * context.log(`Name: ${platform.name}`);
+         * context.log(`Version: ${platform.version}`);
+         * context.log(`Venue: ${platform.venue}`);
+         * context.log(`Description: ${platform.description}`);
+         * context.log(`Serial Number: ${platform.serialnumber}`);
+         * context.log(`Family: ${platform.family}`);
+         * context.log(`Manufacturer: ${platform.manufacturer}`);
+         * context.log(`Model: ${platform.model}`);
+         * context.log(`Label: ${platform.label}`);
+         * ```
+         */
         interface PlatformService {
-            venue: string;
-            serialnumber: string;
+            /**
+             * A string containing the current platform state. ex. "Running"
+             */
             devicestate: string;
-            name: string;
-            description: string;
+
+            /**
+             * A string containing the platform location
+             */
             location: string;
-            model: string;
-            label: string;
-            family: string;
+
+            /**
+             * A string containing the platform name
+             */
+            name: string;
+
+            /**
+             * A string containing the platform version
+             */
             version: string;
+
+            /**
+             * A string containing the platform venue
+             */
+            venue: string;
+
+            /**
+             * A string congaining the platform description
+             */
+            description: string;
+
+            /**
+             * A string containing the platform serial number
+             */
+            serialnumber: string;
+
+            /**
+             * A string containing the platform family
+             */
+            family: string;
+
+            /**
+             * A string containing the platform manufacturer
+             */
             manufacturer: string;
+
+            /**
+             * A string containing the platform model
+             */
+            model: string;
+
+            /**
+             * A string containing the platform label
+             */
+            label: string;
         }
 
         interface DiagnosticsService {}
-        interface NetLinxClientService {}
-        interface SessionService {}
+
+        /**
+         * The *NetLinxClient* service provides a mechanism for scripting language to communicate with a legacy NX controller.
+         */
+        interface NetLinxClientService {
+            online: {
+                listen(callback: () => void): void;
+            };
+            offline: {
+                listen(callback: () => void): void;
+            };
+            string: {
+                listen(callback: () => void): void;
+            };
+            command: {
+                listen(callback: () => void): void;
+            };
+
+            /**
+             * Initiates a connection to NetLinx Controller
+             *
+             * @param {string} host A string containing the destination NetLinx Controller IP address or hostname
+             * @param {number} device A integer containing the local ICSP device number to report to remote controller
+             * @param {string} [username] A string containing the username used for ICSP authentication
+             * @param {string} [password] A string containing the password used for ICSP authentication
+             *
+             * @returns {void} void
+             */
+            connect(
+                host: string,
+                device: number,
+                username?: string,
+                password?: string
+            ): void;
+
+            /**
+             * Closes a connection to a NetLinx Controller
+             *
+             * @returns {void} void
+             */
+            disconnect(): void;
+
+            /**
+             * Sends a command to the device
+             *
+             * @param {string} data The command to send
+             *
+             * @returns {void} void
+             */
+            send_command(data: string): void;
+
+            /**
+             * Sends a string to the device
+             *
+             * @param {string} data The string to send
+             *
+             * @returns {void} void
+             */
+            send_string(data: string): void;
+        }
+
+        /**
+         * The *Session* service provides a mechanism for authenticating users  from within a script.
+         *
+         * Each call into the context's service requests returns the same, single instance of SessionService.
+         */
+        interface SessionService {
+            onLogin: {
+                listen(callback: (event?: SessionLoginEvent) => void): void;
+            };
+            onLogout: {
+                listen(callback: (event?: SessionLogoutEvent) => void): void;
+            };
+            login(username: string, password: string): void;
+            logout(username: string): void;
+        }
+
+        interface SessionLogoutEvent {
+            username: string;
+        }
+
+        interface SessionLoginEvent extends SessionLogoutEvent {
+            status: boolean;
+            statusMsg: string;
+            permissions: string;
+        }
 
         interface SmtpService {
             /**
              * Set the configuration for the SMTP service
-             * @param domain The domain of the SMTP server
-             * @param username The username to authenticate with
-             * @param password The password to authenticate with
-             * @param name The name of the sender
-             * @param port The port to connect to
-             * @param tls Whether to use TLS
-             * @returns void
+             *
+             * @param {string} domain The domain of the SMTP server
+             * @param {string} username The username to authenticate with
+             * @param {string} password The password to authenticate with
+             * @param {string} name The name of the sender
+             * @param {number} port The port to connect to
+             * @param {boolean} tls Whether to use TLS
+             *
+             * @returns {void} void
              */
             setConfig(
                 domain: string,
@@ -238,38 +416,42 @@ declare global {
                 password: string,
                 name: string,
                 port: number,
-                tls: boolean,
+                tls: boolean
             ): void;
 
             /**
              * Get the current configuration of the SMTP service
-             * @returns The current configuration
+             *
+             * @returns {unknown} The current configuration
              */
             getConfig(): unknown;
 
             /**
              * Clear the current configuration of the SMTP service
-             * @returns void
+             *
+             * @returns {void} void
              */
             clearConfig(): void;
 
             /**
-             * Send an email
-             * @param address The email address to send to
-             * @param name The name of the recipient
-             * @param subject The subject of the email
-             * @param body The body of the email
-             * @param attachment The attachment to include
-             * @param fileName The name of the attachment
-             * @returns void
+             * Sends an email
+             *
+             * @param {string} address The email address to send to
+             * @param {string} name The name of the recipient
+             * @param {string} subject The subject of the email
+             * @param {string} body The body of the email
+             * @param {string} [attachment] The attachment to include
+             * @param {string} [fileName] The name of the attachment
+             *
+             * @returns {void} void
              */
             sendEmail(
                 address: string,
                 name: string,
                 subject: string,
                 body: string,
-                attachment: string,
-                fileName: string,
+                attachment?: string,
+                fileName?: string
             ): void;
         }
 
@@ -324,7 +506,7 @@ declare global {
         type ICSPEventCallback = (event: ICSPEvent) => void;
         type ICSPCustomEventCallback = (event: ICSPCustomEvent) => void;
         type ICSPParameterUpdateCallback<T = any> = (
-            event: ParameterUpdate<T>,
+            event: ParameterUpdate<T>
         ) => void;
 
         interface ICSPPort {
